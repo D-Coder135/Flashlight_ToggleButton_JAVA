509e98a25851a682d4f3ce402aa2fcc9163a0d9a
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-05 22:01:20 +0530

    initialize the object for the CameraManager class with the method call of the method getSystemService()

--- app/src/main/java/com/example/flashlight_togglebutton_java/MainActivity.java
+++ app/src/main/java/com/example/flashlight_togglebutton_java/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.flashlight_togglebutton_java;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.ToggleButton;
 
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class MainActivity extends AppCompatActivity {
     ToggleButton mainButton;
+    CameraManager cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
